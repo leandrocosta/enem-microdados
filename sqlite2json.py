959283e8c1613be37d05edd76218b184685ec194
Leandro Costa <leandro.costa@gmail.com>
2014-05-08 16:23:55 -0300

    Add summary table, change Makefiles

--- sqlite2json.py
+++ sqlite2json.py
@@ -9,14 +9,15 @@ simplejson.encoder.c_make_encoder = None
 conn = sqlite3.connect(sys.argv[1])
 c = conn.cursor()
 
-d = []
+qry =""" 
+     SELECT
+         COD_MUNICIPIO_INSC, SUM(INSCRITOS), SUM(PRESENTES),
+         AVG(CN_AVG/PRESENTES), AVG(CH_AVG/PRESENTES),
+         AVG(LC_AVG/PRESENTES), AVG(MT_AVG/PRESENTES),
+         AVG(RE_AVG/PRESENTES), AVG(ME_AVG/PRESENTES)
+     FROM SUMMARY GROUP BY COD_MUNICIPIO_INSC
+     """
 
-for row in c.execute('SELECT cod_municipio_insc, COUNT(1), \
-        COUNT(nu_nt_cn+nu_nt_ch+nu_nt_lc+nu_nt_mt+nu_nota_redacao), \
-        AVG(nu_nt_cn), AVG(nu_nt_ch), AVG(nu_nt_lc), AVG(nu_nt_mt), AVG(nu_nota_redacao), \
-        AVG((nu_nt_cn+nu_nt_ch+nu_nt_lc+nu_nt_mt+nu_nota_redacao)/5) \
-        FROM dados GROUP BY cod_municipio_insc'):
-    attrs = dict(zip(("p", "c", "n", "cn", "ch", "lc", "mt", "re", "me"), row))
-    d.append(attrs)
+d = [list(row) for row in c.execute(qry)]
 
 print simplejson.dumps(d, separators=(',', ':'))#, indent=1)
